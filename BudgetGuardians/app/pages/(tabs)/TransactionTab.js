787c8132862759d34a7af6bd1ee9233a14b931c3
Parrycys <parrychan339@yahoo.com>
2024-06-20 17:36:49 +0800

    Update TransactionTab.js

--- BudgetGuardians/app/pages/(tabs)/TransactionTab.js
+++ BudgetGuardians/app/pages/(tabs)/TransactionTab.js
@@ -1,9 +1,13 @@
 import DropdownComponent from "../../components/expense"
+import { View, Text, StyleSheet } from "react-native";
 
 export default function TransactionTab() {
     return (
         <>
-            <DropdownComponent/>
+            <View style = {[{width:"95%",height:"95%", margin: "5%", alignItems: "center"}]}>
+                <DropdownComponent/>
+            </View>
+            
         </>
     )
 }
