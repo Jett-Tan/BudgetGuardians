b38b5bd6fa4dc447497e08ae924130cba6b5f62a
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2024-06-28 19:27:26 +0800

    Update BudgetTab.js

--- BudgetGuardians/app/pages/(tabs)/BudgetTab.js
+++ BudgetGuardians/app/pages/(tabs)/BudgetTab.js
@@ -6,7 +6,7 @@ export default function BudgetTab() {
     
     return (<>
         <View style = {[{width:"95%",height:"95%",flexDirection:"row", margin: "2.5%", alignItems: "flex-start",flexWrap:"wrap"}]}>
-            <View style={{height:"auto",width:"20%",minWidth:300}}>
+            <View style={{height:"90%",width:"20%",minWidth:300}}>
                 <BudgetInput/>
             </View>
             <View style={{height:"90%", width:"75%",minWidth:300}}>
