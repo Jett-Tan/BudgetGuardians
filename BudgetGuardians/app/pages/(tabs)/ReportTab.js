819ab88a1794a899111d5c30b27e598f590d345f
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2024-07-10 01:01:36 +0800

    Create ReportTab.js

---
+++ BudgetGuardians/app/pages/(tabs)/ReportTab.js
@@ -0,0 +1,9 @@
+import { View , Text} from "react-native";
+
+export default function ReportTab() {
+    return (
+        <View style={{width:"100%",height:"100%",justifyContent:"center",alignItems:"center"}}>
+            <Text style={{color:"white"}}>Coming soon</Text>
+        </View>
+    )
+}
